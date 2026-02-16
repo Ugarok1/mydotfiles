a26de3baf63467a2b243d8c31f8b72c14fb4a8d5
ugarok <mishasutyrin57@gmail.com>
2026-02-16 21:12:54 +0100

    added nvim

--- .config/UGmenu/main.py
+++ .config/UGmenu/main.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-blocks = ["calculator", "calendar", "wallpapers"]
+blocks = ["calculator", "calendar", "wallpapers", "documents"]
 rofi_run = subprocess.run(
     ["rofi", "-dmenu", "-i", "-p", "Select Action"],
     input="\n".join(blocks),
@@ -18,3 +18,5 @@ if rofi_run_output == "calendar":
     subprocess.run(["python3", "/home/myka/.config/UGmenu/popup-calendar.py"])
 if rofi_run_output == "wallpapers":
     os.system("feh --bg-fill --randomize ~/images/wallpapers/")
+if rofi_run_output == "documents":
+    os.system("zathura")
